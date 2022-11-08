083c6c53f4239285037ab9aca586971f392cb59d
kartien <kevinramon523@gmail.com>
2022-11-08 06:08:56 -0500

    Add models

--- apps/book/models.py
+++ apps/book/models.py
@@ -7,6 +7,7 @@ class Author(models.Model):
     surname = models.CharField(max_length=200, blank=False, null=False)
     nationality = models.CharField(max_length=200, blank=False, null=False)
     descriptio = models.TextField(blank=False, null=False)
+    creation_date = models.DateField('Creation Date', auto_now = True, auto_now_add = False)
 
     class Meta:
         verbose_name = "Author"
@@ -21,7 +22,8 @@ class Book(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField('Title', max_length=200, blank=False, null=False)
     publication_date = models.DateField('Publication Date', blank=False, null=False)
-    author_id = models.OneToOneField(Author, on_delete = models.CASCADE)
+    author_id = models.ManyToManyField(Author)
+    creation_date = models.DateField('Creation Date', auto_now = True, auto_now_add = False)
     
     class Meta:
         verbose_name = "Book"
--- command
+++ command
@@ -8,3 +8,8 @@ ver datos de base
 \d
 consultas
 select * from auth_user;
+
+Relaciones 
+cuando de es de uno a uno OneToOneField requiere on_delete = models.CASCADE
+cuandode es es uno a muchos es ForeignKey requiere on_delete = models.CASCADE
+cuando es de nuchos a muchos es ManyToManyField no requiere on_delete = models.CASCADE
